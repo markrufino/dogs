7223a96e299baba74872349b24710ed572758f24
Mac Romel D. Noble <>
2022-10-21 08:16:57 +0800

    WIP add concrete implementation of itemCellViewModel

---
+++ dogs/src/view/cells/DogBreedCellViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  DogBreedCellViewModel.swift
+//  dogs
+//
+//  Created by Mac Romel D. Noble on 10/21/22.
+//
+
+import Foundation
+
+class DogBreedCellViewModel {
+    
+}
+
+// MARK: - ItemCellViewModel
+
+extension DogBreedCellViewModel: ItemCellViewModel {
+    var title: String {
+        "Title"
+    }
+    
+    var subtitle: String? {
+        "Subtitle"
+    }
+    
+}
